546305d6c74035f1ab8d00e6ded3725eff58f1ca
Emrick Sorensen <emrickishere@gmail.com>
2026-04-07 13:48:05 -0600

    More note fingerings

--- data/notes.py
+++ data/notes.py
@@ -21,23 +21,68 @@ class Note:
 
 
 class Notes:
-    B_FLAT_3 = Note(name="B_FLAT_3", midi_number=49, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_B_FLAT)}, staff_y_coord=165, accidental=Accidental.FLAT)
-    B_3 = Note(name="B_3", midi_number=50, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_B)}, staff_y_coord=165)
-    C_4 = Note(name="C_4", midi_number=51, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C)}, staff_y_coord=154)
-    C_SHARP_4 = Note(name="C_SHARP_4", midi_number=52, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_C_SHARP)}, staff_y_coord=154, accidental=Accidental.SHARP)
-    D_4 = Note(name="D_4", midi_number=53, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3)}, staff_y_coord=143)
-    E_FLAT_4 = Note(name="E_FLAT_4", midi_number=54, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_E_FLAT)}, staff_y_coord=132, accidental=Accidental.FLAT)
-    E_4 = Note(name="E_4", midi_number=55, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2)}, staff_y_coord=132)
-    F_4 = Note(name="F_4", midi_number=56, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1)}, staff_y_coord=121)
-    F_SHARP_4 = Note(name="F_4", midi_number=57, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_2), (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_F_SHARP)}, staff_y_coord=121, accidental=Accidental.SHARP)
-    G_4 = Note(name="G_4", midi_number=58, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3)}, staff_y_coord=110)
-    A_FLAT_4 = Note(name="A_FLAT_4", midi_number=59, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.L_G_SHARP)}, staff_y_coord=99, accidental=Accidental.FLAT)
-    A_4 = Note(name="A_4", midi_number=60, fingerings={(Buttons.L_1, Buttons.L_2)}, staff_y_coord=99)
-    B_FLAT_4 = Note(name="B_FLAT_4", midi_number=61, fingerings={(Buttons.L_1, Buttons.L_2, Buttons.R_B_FLAT)}, staff_y_coord=88, accidental=Accidental.FLAT)
-    B_4 = Note(name="B_4", midi_number=62, fingerings={(Buttons.L_1,)}, staff_y_coord=88)
-    C_5 = Note(name="C_5", midi_number=63, fingerings={(Buttons.L_2,)}, staff_y_coord=77)
-    C_SHARP_5 = Note(name="C_SHARP_5", midi_number=64, fingerings={tuple()}, staff_y_coord=77, accidental=Accidental.SHARP)
-    D_5 = Note(name="D_5", midi_number=65, fingerings={(Buttons.L_OCTAVE,) + btns for btns in D_4.button_fingerings}, staff_y_coord=66)
+    B_FLAT_3 = Note(name="B_FLAT_3", midi_number=49, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_B_FLAT)
+    }, staff_y_coord=165, accidental=Accidental.FLAT)
+    B_3 = Note(name="B_3", midi_number=50, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_B)
+    }, staff_y_coord=165)
+
+    C_4 = Note(name="C_4", midi_number=51, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C)
+    }, staff_y_coord=154)
+    C_SHARP_4 = Note(name="C_SHARP_4", midi_number=52, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_C, Buttons.L_LOW_C_SHARP)
+    }, staff_y_coord=154, accidental=Accidental.SHARP)
+    D_4 = Note(name="D_4", midi_number=53, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3)
+    }, staff_y_coord=143)
+    E_FLAT_4 = Note(name="E_FLAT_4", midi_number=54, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2, Buttons.R_3, Buttons.R_LOW_E_FLAT)
+    }, staff_y_coord=132, accidental=Accidental.FLAT)
+    E_4 = Note(name="E_4", midi_number=55, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_2)
+    }, staff_y_coord=132)
+    F_4 = Note(name="F_4", midi_number=56, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1)
+    }, staff_y_coord=121)
+    F_SHARP_4 = Note(name="F_4", midi_number=57, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_2),
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.R_1, Buttons.R_F_SHARP)
+    }, staff_y_coord=121, accidental=Accidental.SHARP)
+    G_4 = Note(name="G_4", midi_number=58, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3)
+    }, staff_y_coord=110)
+    A_FLAT_4 = Note(name="A_FLAT_4", midi_number=59, fingerings={
+        (Buttons.L_1, Buttons.L_2, Buttons.L_3, Buttons.L_G_SHARP)
+    }, staff_y_coord=99, accidental=Accidental.FLAT)
+    A_4 = Note(name="A_4", midi_number=60, fingerings={
+        (Buttons.L_1, Buttons.L_2)
+    }, staff_y_coord=99)
+    B_FLAT_4 = Note(name="B_FLAT_4", midi_number=61, fingerings={
+        (Buttons.L_OCTAVE,) + btns for btns in B_FLAT_3.button_fingerings,
+        (Buttons.L_1, Buttons.L_2, Buttons.R_B_FLAT),
+        (Buttons.L_1, Buttons.L_B_FLAT),
+        (Buttons.L_1, Buttons.R_1),
+        (Buttons.L_1, Buttons.R_2),
+    }, staff_y_coord=88, accidental=Accidental.FLAT)
+    B_4 = Note(name="B_4", midi_number=62, fingerings={
+        (Buttons.L_OCTAVE,) + btns for btns in B_3.button_fingerings,
+        (Buttons.L_1,)
+    }, staff_y_coord=88)
+
+    C_5 = Note(name="C_5", midi_number=63, fingerings={
+        (Buttons.L_OCTAVE,) + btns for btns in C_4.button_fingerings,
+        (Buttons.L_2,),
+        (Buttons.L_1, Buttons.R_C)
+    }, staff_y_coord=77)
+    C_SHARP_5 = Note(name="C_SHARP_5", midi_number=64, fingerings={
+        (Buttons.L_OCTAVE,) + btns for btns in C_SHARP_4.button_fingerings,
+        tuple()
+    }, staff_y_coord=77, accidental=Accidental.SHARP)
+    D_5 = Note(name="D_5", midi_number=65, fingerings={
+        (Buttons.L_OCTAVE,) + btns for btns in D_4.button_fingerings
+    }, staff_y_coord=66)
     E_FLAT_5 = Note(name="E_FLAT_5", midi_number=66, fingerings={(Buttons.L_OCTAVE,) + btns for btns in E_FLAT_4.button_fingerings}, staff_y_coord=55, accidental=Accidental.FLAT)
     E_5 = Note(name="E_5", midi_number=67, fingerings={(Buttons.L_OCTAVE,) + btns for btns in E_4.button_fingerings}, staff_y_coord=55)
     F_5 = Note(name="F_5", midi_number=68, fingerings={(Buttons.L_OCTAVE,) + btns for btns in F_4.button_fingerings}, staff_y_coord=44)
@@ -47,12 +92,28 @@ class Notes:
     A_5 = Note(name="A_5", midi_number=72, fingerings={(Buttons.L_OCTAVE,) + btns for btns in A_4.button_fingerings}, staff_y_coord=22)
     B_FLAT_5 = Note(name="B_FLAT_5", midi_number=73, fingerings={(Buttons.L_OCTAVE,) + btns for btns in B_FLAT_4.button_fingerings}, staff_y_coord=11, accidental=Accidental.FLAT)
     B_5 = Note(name="B_5", midi_number=74, fingerings={(Buttons.L_OCTAVE,) + btns for btns in B_4.button_fingerings}, staff_y_coord=11)
+
     C_6 = Note(name="C_6", midi_number=75, fingerings={(Buttons.L_OCTAVE,) + btns for btns in C_5.button_fingerings}, staff_y_coord=0)
-    C_SHARP_6 = Note(name="C_SHARP_6", midi_number=76, fingerings={(Buttons.L_OCTAVE,)}, staff_y_coord=0, accidental=Accidental.SHARP)
-    D_6 = Note(name="D_6", midi_number=77, fingerings={(Buttons.L_OCTAVE, Buttons.L_D)}, staff_y_coord=-11)
-    E_FLAT_6 = Note(name="E_FLAT_6", midi_number=78, fingerings={(Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT)}, staff_y_coord=-22, accidental=Accidental.FLAT)
-    E_6 = Note(name="E_6", midi_number=79, fingerings={(Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT, Buttons.R_E)}, staff_y_coord=-22)
-    F_6 = Note(name="F_6", midi_number=80, fingerings={(Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT, Buttons.L_F, Buttons.R_E)}, staff_y_coord=-33)
+    C_SHARP_6 = Note(name="C_SHARP_6", midi_number=76, fingerings={
+        (Buttons.L_OCTAVE,)
+    }, staff_y_coord=0, accidental=Accidental.SHARP)
+    D_6 = Note(name="D_6", midi_number=77, fingerings={
+        (Buttons.L_OCTAVE, Buttons.L_D)
+    }, staff_y_coord=-11)
+    E_FLAT_6 = Note(name="E_FLAT_6", midi_number=78, fingerings={
+        (Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT)
+    }, staff_y_coord=-22, accidental=Accidental.FLAT)
+    E_6 = Note(name="E_6", midi_number=79, fingerings={
+        (Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT, Buttons.R_E),
+        (Buttons.L_OCTAVE, Buttons.L_2, Buttons.L_3, Buttons.L_FRONT_F)
+    }, staff_y_coord=-22)
+    F_6 = Note(name="F_6", midi_number=80, fingerings={
+        (Buttons.L_OCTAVE, Buttons.L_D, Buttons.L_E_FLAT, Buttons.L_F, Buttons.R_E),
+        (Buttons.L_OCTAVE, Buttons.L_2, Buttons.L_FRONT_F)
+    }, staff_y_coord=-33),
+    F_SHARP_6 = Note(name="F_SHARP_6", midi_number=81, fingerings={
+        (Buttons.L_OCTAVE, Buttons.L_2, Buttons.L_FRONT_F, Buttons.R_HIGH_F_SHARP)
+    }, staff_y_coord=-33, accidental=Accidental.SHARP)
 
     ALL = (B_FLAT_3, B_3,
            C_4, C_SHARP_4, D_4, E_FLAT_4, E_4, F_4, F_SHARP_4, G_4, A_FLAT_4, A_4, B_FLAT_4, B_4,
